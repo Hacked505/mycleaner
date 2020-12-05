8fcb75f37c91656382f3b92e9c9cd3661181e04c
Aleksandr Suvorov <myhackband@gmail.com>
2020-12-05 12:17:35 +0700

    Added COLUMNS - shutil.get_terminal_size

--- mycleaner/term_my_cleaner.py
+++ mycleaner/term_my_cleaner.py
@@ -1,11 +1,16 @@
 import sys
+import shutil
 import os
 from pathlib import Path
 
 try:
     from mycleaner import cleaner, smart
 except ModuleNotFoundError:
-    import cleaner, smart
+    import cleaner
+    import smart
+
+
+COLUMNS, _ = shutil.get_terminal_size()
 
 
 def get_path():
@@ -19,13 +24,13 @@ def make_path_obj(path_list):
 
 def logo_dec(func):
     def deco():
-        print('My Cleaner'.center(79, '='))
-        print('Утилита для затирания, обнуления, удаления файлов'.center(79, ' '))
-        print('Aleksandr Suvorov | myhackband@ya.ru'.center(79, '-'))
-        print(''.center(79, '='))
+        print('My Cleaner'.center(COLUMNS, '='))
+        print('Утилита для затирания, обнуления, удаления файлов'.center(COLUMNS, ' '))
+        print('Aleksandr Suvorov | myhackband@ya.ru'.center(COLUMNS, '-'))
+        print(''.center(COLUMNS, '='))
         func()
-        print(''.center(79, '='))
-        print('Программа завершена'.center(79, '-'))
+        print(''.center(COLUMNS, '='))
+        print('Программа завершена'.center(COLUMNS, '-'))
     return deco
 
 
@@ -40,22 +45,22 @@ def main():
         my_cleaner = cleaner.Cleaner()
         num_files = sum([len(files) for path in path_list for p, _, files in os.walk(path)])
         print(f'Найдено файлов: {num_files}')
-        print(''.center(78, '='))
+        print(''.center(COLUMNS, '='))
         while True:
             print('Выберите нужное действие:\n0. Выход\n1. Уничтожение\n2. Обнуление\n3. Обычное удаление')
-            print(''.center(78, '-'))
+            print(''.center(COLUMNS, '-'))
             try:
                 user_input = int(input('Ввод: '))
                 if user_input not in [0, 1, 2, 3]:
                     raise ValueError
                 my_cleaner.shreds = int(input('Введите количество проходов для перезаписи: '))
             except ValueError:
-                print(''.center(78, '-'))
+                print(''.center(COLUMNS, '-'))
                 print('Ошибка ввода!')
                 continue
             else:
                 for obj in obj_list:
-                    print(f'Работаем с: {obj.path}'.center(79, '='))
+                    print(f'Работаем с: {obj.path}'.center(COLUMNS, '='))
                     for file in obj.get_files():
                         if user_input == 1:
                             print(f'Уничтожаем файл: {file}')
@@ -66,7 +71,7 @@ def main():
                         elif user_input == 3:
                             print(f'Удаляем файл: {file}')
                             my_cleaner.del_file(file)
-            print('Работа завершена'.center(78, '='))
+            print('Работа завершена'.center(COLUMNS, '='))
             print(f'Было обработано файлов: {my_cleaner.count_del_files + my_cleaner.count_zero_files}')
             break
     else:
